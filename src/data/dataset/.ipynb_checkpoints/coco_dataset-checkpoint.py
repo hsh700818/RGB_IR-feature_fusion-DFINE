d8e446550494e56faa5afe4501ae599a203280a4
hsh700818 <hsh700818@163.com>
2026-04-27 19:04:53 +0800

    整体可运行，还差处理 rotated_iou() 和 rbox_to_adr_target()以及DN还没完全适配5维OBB，旋转框IoU损失为0

--- src/data/dataset/.ipynb_checkpoints/coco_dataset-checkpoint.py
+++ src/data/dataset/.ipynb_checkpoints/coco_dataset-checkpoint.py
@@ -2,7 +2,7 @@ import os
 import torch
 import torchvision
 import torchvision.transforms.functional as F
-from PIL import Image
+from PIL import Image, ImageFile
 import numpy as np
 
 from ...core import register
@@ -12,6 +12,7 @@ from faster_coco_eval.utils.pytorch import FasterCocoDetection
 
 torchvision.disable_beta_transforms_warning()
 Image.MAX_IMAGE_PIXELS = None
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 __all__ = ["CocoDetection", "mscoco_category2name", "mscoco_category2label", "mscoco_label2category"]
 
